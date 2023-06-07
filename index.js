b4fadb856988cb6a112d3c25d497dd99115bb30a
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-06-07 14:04:52 -0300

    Quitamos cors

--- index.js
+++ index.js
@@ -10,17 +10,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 
-const whitelist = ['http://18.191.55.134:3000/red/user', 'http://localhost:3000/#/', 'http://localhost:3000', 'http://abrilgonzalez-com.stackstaging.com', 'http://abrilgonzalez-com.stackstaging.com/', 'http://abrilgonzalez-com.stackstaging.com/#/', 'http://abrilgonzalez-com.stackstaging.com/#', 'http://localhost:3000/?#/profile', 'http://localhost:3000'];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
-    }
-  }
-}
-app.use(cors(options));
+
+app.use(cors());
 
 app.use(logErrors);
 app.use(ormErrorHandler);
