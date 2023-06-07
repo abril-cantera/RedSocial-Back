97ab8fb3fc94fe9e745842d3372260bc80133c17
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2023-06-07 15:03:07 -0300

    Deploy 2

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,7 +5,7 @@ const userRouter = require('./user.router');
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1/', router);
-  router.use('/user', userRouter);
+  router.use('/user', userRouter)
 }
 
 module.exports = routerApi;
